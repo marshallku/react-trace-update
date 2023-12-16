cf1fe6d23d00cc1fab0f49236b42f9905f0f2206
Marshall Ku <marshall@kakao.com>
2023-12-16 19:59:05 +0900

    Add build configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
+import pkg from "./package.json" assert { type: "json" };
+
+const makeExternalPredicate = (externalArr: string[]) => {
+    if (externalArr.length === 0) {
+        return () => false;
+    }
+    const pattern = new RegExp(`^(${externalArr.join("|")})($|/)`);
+    return (id: string) => pattern.test(id);
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    build: {
+        lib: {
+            entry: "src/index.ts",
+            name: "react-trace-update",
+            formats: ["es", "cjs"],
+            fileName: (format) => `react-trace-update.${format}.js`,
+        },
+        rollupOptions: {
+            external: makeExternalPredicate([
+                ...Object.keys(pkg.peerDependencies),
+            ]),
+        },
+    },
+    plugins: [react(), dts({ include: ["src/index.ts", "src/lib"] })],
+});
